4a2e62bcfa18abac63665a151d711ab230e2532c
Johannes Meintjes <jmeintjesn7@gmail.com>
2023-02-06 19:45:52 +0200

    Added reporting and scheduling

---
+++ tests/demoqa/schedules/runAllTests.ps1
@@ -0,0 +1,5 @@
+﻿$date = Get-Date -Format "dd_MM_yyyy"
+
+cd C:\Users\PatatN7\Documents\TestCafe_Practice\
+
+testcafe chrome .\tests\demoqa\testScripts\ --reporter html:.\tests\demoqa\reports\regression_$date.html
